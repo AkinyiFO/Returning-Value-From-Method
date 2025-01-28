d36c7bc7292db74ef73667d29b7e5b1d2d72a291
AkinyiFO <oafridah@gmail.com>
2025-01-28 18:56:02 +0300

    "Initial"

---
+++ src/main/java/dev/labs/s3/ReturningValueFromMethod.java
@@ -0,0 +1,36 @@
+package dev.labs.s3;
+
+public class ReturningValueFromMethod {
+
+    public static void main(String[] args) {
+
+        ReturningValueFromMethod test = new ReturningValueFromMethod();
+
+        test.completesAllStatements(); // Invoked method
+        System.out.println("Continues after invoked completesAllStatements() returns to this code.");
+
+        test.returnsAValue(5,2); // Invoked method
+        System.out.println("Continues after invoked returnsAValue() returns to this code.");
+
+    }
+
+    void completesAllStatements(){
+        System.out.println("Statement one");
+        System.out.println("Statement two");
+        System.out.println("Last statement");
+        return; // Redundant but allowed
+//        return 6; // Compile-time error
+    }
+
+    int returnsAValue(int x, int y){
+        System.out.println("Before return statement.");
+        return x+y; // Required
+    }
+
+    // Can also return a reference datatype
+    Database bestDatabase(Database oracle, Database postgres) {
+        // Code to compare performance scores
+        return postgres;
+    }
+
+}
